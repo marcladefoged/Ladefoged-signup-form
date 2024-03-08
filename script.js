720f7c51e2a46a831c0604a6fe05c22299a11813
marcladefoged <marcladefogedjensen@gmail.com>
2024-03-08 19:30:01 +0100

    confirm password done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Timestamp: 23 min 33 sek
+
 const form = document.querySelector("form"),
         emailField = form.querySelector(".email-field"),
         emailInput = emailField.querySelector(".email"),
@@ -14,12 +16,59 @@ const form = document.querySelector("form"),
         }
         emailField.classList.remove("invalid");
     }
-        
+
+    // Hide and show password
+    const eyeIcons = document.querySelectorAll(".show-hide");
+    // console.log(eyeIcons);
+
+    eyeIcons.forEach(eyeIcon => {
+        eyeIcon.addEventListener("click", () => {
+            const pInput = eyeIcon.parentElement.querySelector("input"); // getting parent element of eye icon and selecting the password input
+            if (pInput.type === "password") {
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return (pInput.type = "text");
+            }
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+                pInput.type = "password";
+        })
+    })
+
+    // Password Validation
+    function createPass() {
+        // const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/ // String only allowing small and capitalized letters and numbers
+        if (!passInput.value.match(passPattern)) {
+            return passField.classList.add("invalid"); // adding invalid class if password input value do not match with passPattern
+        }
+        passField.classList.remove("invalid"); // removing invalid class if password input value matched with passPattern
+    }
+
+    //  Confirm Password Validation 
+
+    function confirmPass() {
+        if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+            return cPassField.classList.add("invalid");
+        }
+        cPassField.classList.remove("invalid");
+    }
+
     // Calling Function on Form Submit
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         checkEmail();
+        createPass();
+        confirmPass();
 
         // Calling function on keyup
         emailInput.addEventListener("keyup", checkEmail);
+        passInput.addEventListener("keyup", createPass);
+        cPassInput.addEventListener("keyup", confirmPass);
+
+        if (
+        !emailField.classList.contains("invalid") &&
+        !passField.classList.contains("invalid") &&
+        !cPassField.classList.contains("invalid")
+         ) {
+            alert("You have now signed up!")
+        }
     });
